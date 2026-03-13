0f4c6e589b5c8e14cb5400f83dbbdf970a246f9a
tmwang205 <trtr2k5@gmail.com>
2026-03-13 08:31:54 +0700

    video 29

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  //data-type
+  //string
+  const [name, setName] = useState<string>("Xin chào bạn");
+
+  //number
+  const [age, setAge] = useState<number>(21);
+
+  //null, undefined, boolean
+  const test = null;
+
+  //object 
+  const [person, setPerson] = useState({
+    name: "Quang",
+    age: 21
+  });
+
+  //array
+  const [person2, setPerson2] = useState([{
+    name: "Quang",
+    age: 21
+  }])
+
   return (
     <View style={styles.container}>
       <View>
-        <Text>Tôi là người mới</Text>
+        <Text style={styles.test1}>{name}</Text>
+        <Text style={styles.test1}>{JSON.stringify(person)}</Text>
       </View>
       <Text style={styles.test1}>Hello world with
         <Text style={styles.test2}>Quang</Text>
@@ -18,9 +42,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 
   test1: {
